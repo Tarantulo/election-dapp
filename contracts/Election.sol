8de33784e11e37554abf38a3a05f02511473d6fe
Rytis Bigenis <rytisbig@gmail.com>
2018-07-08 21:44:33 +0300

    Voting Enabled

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,9 +8,12 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
+
     // Store Candidates Count
     uint public candidatesCount;
 
@@ -22,6 +25,20 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // must not have voted before
+        require(!voters[msg.sender]);
+        
+        // check for valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);    
+        // record that voter has voted
+
+        voters[msg.sender] = true;
+
+        //update Candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 
 }
